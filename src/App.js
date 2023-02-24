db13724dea7479bb19119fda14f9e0132e4adcca
IngDavidTM <david-1011@hotmail.com>
2023-02-24 09:54:27 -0500

    Configure React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
